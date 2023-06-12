1abfacf4e1fc6661bbc3d469ce5d996b956219e6
maryammolaei <maryam.molaeiii@gmail.com>
2023-06-12 13:19:41 +0330

    AddUnitOfWorkClassToDataLayer

---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using UrlShortener.Service;
+using UrlShortener.Service.Repositories;
+
+namespace UrlShortener.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IUrlShortenerRepository UrlShortenerRepository { get; private set; }
+        private readonly UrlShortenetContext _context;
+
+        public UnitOfWork(UrlShortenetContext context)
+        {
+            _context = context;
+            UrlShortenerRepository = new UrlShortenerRepository(_context);
+        }
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
