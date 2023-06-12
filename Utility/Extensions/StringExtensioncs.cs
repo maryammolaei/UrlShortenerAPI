dc1650b512548c9dbdb8e9b20dbdd18024fec73d
maryammolaei <maryam.molaeiii@gmail.com>
2023-06-12 13:57:02 +0330

    DefineIsValidUrlExtensionMethodToUtilityLayer

---
+++ Utility/Extensions/StringExtensioncs.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Core;
+using System.Runtime.CompilerServices;
+
+namespace UrlShortener.Utility.Extensions
+{
+    public static class StringExtensioncs
+    {
+        public static bool IsValidUrl(this string url)
+        {
+            Uri urlOrigin;
+            return Uri.TryCreate(url, UriKind.Absolute, out urlOrigin);
+        }
+    }
+}
