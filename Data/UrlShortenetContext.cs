1f50182f45b14a1c58147a0ebcaca50d7e663795
maryammolaei <maryam.molaeiii@gmail.com>
2023-06-12 11:26:14 +0330

    AddUrlManamgementClassToModelAndAddUrlShortenetContextInDataLayer

---
+++ Data/UrlShortenetContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Models;
+
+namespace UrlShortener.Data
+{
+    public class UrlShortenetContext : DbContext
+    {
+        public UrlShortenetContext(DbContextOptions<UrlShortenetContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UrlManagement>().ToTable(nameof(UrlManagement));
+            modelBuilder.Entity<UrlManagement>().Property(p => p.Url).HasMaxLength(200);
+            modelBuilder.Entity<UrlManagement>().Property(p => p.ShortUrl).HasMaxLength(50);
+        }
+        public DbSet<UrlManagement> UrlManagement { get; set; }
+    }
+}
